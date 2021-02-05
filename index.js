5cf067cbfa94243160ccd6a66c0c8bcdb1ae7598
chj_damon <chjdamon@gmail.com>
2021-02-05 14:43:59 +0800

    feat: 修改sentry为公司测试环境地址

--- index.js
+++ index.js
@@ -10,20 +10,7 @@ import '@/api';
 enableES5();
 
 Sentry.init({
-  dsn: 'https://0bd170cf72a04e12ac61a19ade59a909@o503055.ingest.sentry.io/5587578',
+  dsn: 'http://4871ac1b6eb442c997d3940e475db957@60.12.241.84:29177/4',
 });
-
-console.disableYellowBox = true;
-
-/**这段代码加上之后，才可以在debug的时候看到网络请求 */
-global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
-global.FormData = global.originalFormData || global.FormData;
-
-if (window.FETCH_SUPPORT) {
-  window.FETCH_SUPPORT.blob = false;
-} else {
-  global.Blob = global.originalBlob || global.Blob;
-  global.FileReader = global.originalFileReader || global.FileReader;
-}
 
 AppRegistry.registerComponent(appName, () => App);
